e11e25e1f8cab191ec30e98e84b651b2db6b1aa4
Vitor-Kemuel <kemuvitor@gmail.com>
2021-09-10 01:04:48 -0300

    Refatorando redes sociais e cards

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View, Image, SafeAreaView, TouchableOpacity, Alert } from 'react-native';
+import { StyleSheet, Text, View, Image, SafeAreaView, TouchableOpacity, Alert, ScrollView } from 'react-native';
 import Icon from 'react-native-vector-icons/Feather';
 
 import foto from './img/perfil_image.jpg'
@@ -20,22 +20,29 @@ export default function App() {
   }
 
   return (
-    <View style={styles.page}>
+    <ScrollView style={styles.page}>
       <View style={styles.cabecalho}>
         <Image source={foto} style={styles.foto} />
         <Text style={styles.nome}>Vitor Kemuel</Text>
         <Text styles={styles.funcao}>Graduando ADS (4/6)</Text>
         <View style={styles.social}>
           <TouchableOpacity onPress={() => handleSocialLink('github')}>
-          <Icon name="github" size={30} />
-            </TouchableOpacity>
+            <View style={styles.social_icon}>
+              <Icon name="github" size={30} />
+              <Text>Vitor-Kemuel</Text>
+            </View>
+          </TouchableOpacity>
           <TouchableOpacity onPress={() => handleSocialLink('linkedin')}>
-            <Icon name="linkedin" size={30} />
+            <View style={styles.social_icon}>
+              <Icon name="linkedin" size={30} />
+              <Text>Vitor-Kemuel</Text>
+            </View>
           </TouchableOpacity>
         </View>
       </View>
 
       <View style={styles.bory}>
+
         <View style={styles.card}>
           <View style={styles.card_header}>
             <Text style={styles.card_title}>Formação</Text>
@@ -43,13 +50,34 @@ export default function App() {
           <View style={styles.card_content}>
             <Text style={styles.info}>
               Tecnologia em analise e desenvolvimento de sistemas{"\n"}
-              <Text style={styles.subinfo}>FATEC Rio Preto - Cursando 4ºPeriodo.</Text>
+              <Text style={styles.subinfo}>FATEC Rio Preto - Cursando 4ºPeriodo</Text>
             </Text>
           </View>
         </View>
+
+        <View style={styles.card}>
+          <View style={styles.card_header}>
+            <Text style={styles.card_title}>Habilidades</Text>
+          </View>
+          <View style={styles.card_content}>
+            <Text style={styles.info}>
+              Back-End{"\n"}
+              <Text style={styles.subinfo}>PHP{"\n"}</Text>
+              <Text style={styles.subinfo}>Python{"\n"}</Text>
+              <Text style={styles.subinfo}>JavaScript{"\n"}</Text>
+            </Text>
+            <Text style={styles.info}>
+              Front-End{"\n"}
+              <Text style={styles.subinfo}>HTML 5{"\n"}</Text>
+              <Text style={styles.subinfo}>CSS 3{"\n"}</Text>
+              <Text style={styles.subinfo}>React Native</Text>
+            </Text>
+          </View>
+        </View>
+
       </View>
       <StatusBar style="auto" />
-    </View>
+    </ScrollView>
   );
 }
 
@@ -80,8 +108,12 @@ const styles = StyleSheet.create({
   social: {
     flexDirection: 'row',
     justifyContent: 'space-between',
-    width: '20%',
-    marginTop: 20
+    width: '50%',
+    alignItems: 'center',
+    marginTop: 20,
+  },
+  social_icon:{
+    alignItems: 'center'
   },
   bory:{
     alignItems: 'center',
@@ -89,12 +121,13 @@ const styles = StyleSheet.create({
   },
   card: {
     width: '90%',
-    borderRadius:5,
+    borderRadius:7,
     borderWidth: 3,
     borderColor: '#808080',
     alignItems:'center',
     padding: 10,
     backgroundColor: '#ffff',
+    marginBottom: 20
   },
   card_title:{
     fontWeight: 'bold',
@@ -104,9 +137,10 @@ const styles = StyleSheet.create({
   },
   info:{
     color: '#000',
-    fontSize: 17
+    fontSize: 17,
+    
   },
   subinfo:{
-    color: '#939393'
-  }
+    color: '#939393',
+  },
 });
